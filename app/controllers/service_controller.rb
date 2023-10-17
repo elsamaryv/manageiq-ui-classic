7d0f59f368fdfdcf3a4eef909a294c332534d233
David Resende <david.resende@ibm.com>
2023-10-17 15:43:14 -0400

    Update Service Reconfigure Form to React

--- app/controllers/service_controller.rb
+++ app/controllers/service_controller.rb
@@ -75,6 +75,9 @@ class ServiceController < ApplicationController
 
   def edit
     assert_privileges("service_edit")
+
+    $log.warn("testing edit")
+
     checked = find_checked_items
     checked[0] = params[:id] if checked.blank? && params[:id]
     @service = find_record_with_rbac(Service, checked[0])
@@ -85,12 +88,12 @@ class ServiceController < ApplicationController
   def service_reconfigure
     assert_privileges('service_reconfigure')
 
-    service = find_record_with_rbac(Service, params[:id])
-    service_template = service.service_template
+    @service = find_record_with_rbac(Service, params[:id])
+    service_template = @service.service_template
     resource_action = service_template.resource_actions.find_by(:action => 'Reconfigure') if service_template
-    @dialog_locals = {:resource_action_id => resource_action.id, :target_id => service.id}
+    @dialog_locals = {:resource_action_id => resource_action.id, :target_id => @service.id}
     @in_a_form = true
-    drop_breadcrumb(:name => _("Reconfigure Service \"%{name}\"") % {:name => service.name}, :url => "/service/service_reconfigure/#{service.id}")
+    drop_breadcrumb(:name => _("Reconfigure Service \"%{name}\"") % {:name => @service.name}, :url => "/service/service_reconfigure/#{@service.id}")
   end
 
   def service_form_fields
@@ -222,8 +225,8 @@ class ServiceController < ApplicationController
       action = "retire"
     when "reconfigure_dialog"
       partial = "shared/dialogs/reconfigure_dialog"
-      header = @right_cell_text
-      action = nil
+      header = _("Reconfigure Service \"%{name}\"") % {:name => @service.name}
+      action = "reconfigure_dialog"
     when "service_edit"
       partial = "service_form"
       header = _("Editing Service \"%{name}\"") % {:name => @service.name}
