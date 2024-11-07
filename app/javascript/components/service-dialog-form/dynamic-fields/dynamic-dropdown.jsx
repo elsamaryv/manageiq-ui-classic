e8beaf8e9c0d1795d86e550b159fbf309acbb67f
Elsa Mary <elsa.mary@ibm.com>
2024-11-07 17:53:39 +0530

    Adds checkbox, radiobutton, datepicker and dropdown (WIP)

---
+++ app/javascript/components/service-dialog-form/dynamic-fields/dynamic-dropdown.jsx
@@ -0,0 +1,90 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Dropdown, FormLabel } from 'carbon-components-react';
+import { dynamicFieldDataProps, SD_ACTIONS } from '../helper';
+import DynamicFieldActions from '../dynamic-field-actions';
+import {
+  fieldInformation, advanced, overridableOptions, fieldTab, dynamicFields,
+} from './dynamic-field-configuration';
+
+/** Component to render a Field. */
+const DynamicDropdown = ({ dynamicFieldData: { section, field, fieldPosition }, onFieldAction }) => {
+  const { tabId, sectionId } = section;
+  const fieldActions = (event, type) => onFieldAction({
+    event,
+    fieldPosition,
+    type,
+  });
+
+  const dynamicDropdownOptions = () => ([
+    dynamicFields.entryPoint,
+    dynamicFields.showRefresh,
+    dynamicFields.loadOnInit,
+    dynamicFields.required,
+    dynamicFields.protected,
+    dynamicFields.valueType,
+    dynamicFields.validation,
+    dynamicFields.validator,
+    dynamicFields.fieldsToRefresh,
+    dynamicFields.multiselect,
+  ]);
+
+  const ordinaryDropdownOptions = () => ([
+    dynamicFields.defaultValue,
+    dynamicFields.required,
+    dynamicFields.protected,
+    dynamicFields.readOnly,
+    dynamicFields.visible,
+    dynamicFields.valueType,
+    dynamicFields.validation,
+    dynamicFields.validator,
+    dynamicFields.fieldsToRefresh,
+    dynamicFields.multiselect,
+  ]);
+
+  const DropdownOptions = (dynamic) => ({
+    name: fieldTab.options,
+    fields: dynamic ? dynamicDropdownOptions() : ordinaryDropdownOptions(),
+  });
+
+  const DropdownEditFields = (dynamic) => {
+    const tabs = [
+      fieldInformation(),
+      DropdownOptions(dynamic),
+      advanced(),
+    ];
+    if (dynamic) {
+      tabs.push(overridableOptions());
+    }
+    return tabs;
+  };
+
+  return (
+    <div className="dynamic-form-field">
+      <div className="dynamic-form-field-item">
+        <FormLabel>
+          Dropdown
+        </FormLabel>
+          {/* <Dropdown
+            id={`tab-${tabId}-section-${sectionId}-field-${fieldPosition}-dropdown`}
+            name={`tab-${tabId}-section-${sectionId}-field-${fieldPosition}-dropdown`}
+            titleText={__('Dropdown')}
+            label=""
+            onChange={(event) => fieldActions(event, SD_ACTIONS.dropdownOnChange)}
+          /> */}
+      </div>
+      <DynamicFieldActions
+        componentId={field.componentId}
+        dynamicFieldAction={(action) => console.log(action, field)}
+        fieldConfiguration={DropdownEditFields(false)}
+      />
+    </div>
+  );
+};
+
+DynamicDropdown.propTypes = {
+  dynamicFieldData: dynamicFieldDataProps.isRequired,
+  onFieldAction: PropTypes.func.isRequired,
+};
+
+export default DynamicDropdown;
