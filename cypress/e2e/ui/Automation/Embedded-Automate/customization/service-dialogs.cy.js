61e043185935520376d04140402e180d952aef62
Elsa Mary <elsa.mary@ibm.com>
2025-07-31 18:02:48 +0530

    adds more tests on section

--- cypress/e2e/ui/Automation/Embedded-Automate/customization/service-dialogs.cy.js
+++ cypress/e2e/ui/Automation/Embedded-Automate/customization/service-dialogs.cy.js
@@ -54,9 +54,10 @@ describe('Automation > Embedded Automate > Customization > Service Dialogs', () 
       });
     });
 
+    // Tabs
     it('Ensure 2 tabs are seen initially where one indicates - create a new tab', () => {
       cy.get('#dynamic-tabs')
-        .find('ul[role = "tablist"]').should('exist')
+        .find('ul[role="tablist"]').should('exist')
         .within(() => {
           // Ensure there are exactly 2 <li>
           cy.get('li')
@@ -65,6 +66,45 @@ describe('Automation > Embedded Automate > Customization > Service Dialogs', () 
           cy.get('li').first().find('button').invoke('text').should('eq', 'New Tab');
           // Last li has button with "create"
           cy.get('li').last().find('button').invoke('text').should('eq', 'Create Tab');
+        });
+    });
+
+    // Sections
+    it.only('Ensure first tab has a section with 3 action buttons', () => {
+      cy.get('.dynamic-tabs-wrapper')
+        .find('div[role="tabpanel"]').should('exist')
+        .find('.dynamic-sections-wrapper .dynamic-section')
+        .should('have.attr', 'id', 'dynamic-tab-0-section-0')
+        .find('.dynamic-section-title').should('have.text', 'New Section');
+
+      // checks text on the action buttons
+      cy.get('.dynamic-section .dynamic-section-actions')
+        .within(() => {
+          cy.get('button').should('have.length', 3)
+            .each(($el) => {
+              cy.wrap($el)
+                .invoke('attr', 'title')
+                .then((text) => {
+                  expect(['Minimize', 'Edit section', 'Remove section']).to.include(text.trim());
+                });
+            });
+        });
+
+      // checks for the message inside empty section
+      cy.get('.dynamic-section .dynamic-section-contents')
+        .invoke('text')
+        .should('eq', 'Drag items here to add to the dialog. At least one item is required before saving');
+
+      // collapse a section
+      cy.get('.dynamic-section-actions')
+        .find('button[title="Minimize"]').click()
+        .then(() => {
+          cy.get('.dynamic-section-contents').should('not.exist');
+        });
+      // Expand again
+      cy.get('button[title="Maximize"]').click()
+        .then(() => {
+          cy.get('.dynamic-section-contents').should('exist');
         });
     });
   });
